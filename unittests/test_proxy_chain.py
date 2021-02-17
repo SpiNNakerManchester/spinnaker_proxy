1981e3d37d5eba2b3e6a74a06eeb8d275e3345d1
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2021-02-17 13:57:31 +0000

    flake8, rat

--- unittests/test_proxy_chain.py
+++ unittests/test_proxy_chain.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2017-2021 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from functools import partial
 import struct
 import threading
@@ -17,6 +32,7 @@ class Runner:
     shutting that all down neatly.
     """
     __slots__ = ["_target", "_stopper", "_thread", "_exn"]
+
     def __init__(self, target):
         self._target = target
         self._stopper = threading.Event()
